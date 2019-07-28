c5514b4294f25be535eed3741b3bf31d8e824226
tarahi <sakurai2637@gmail.com>
2019-07-28 11:32:50 +0900

    比較演算子の変更

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ const input = prompt('今日の天気は？次の3つのうち、いずれかの
 //   - elseを使う
 //
 // alertのドキュメント: https://developer.mozilla.org/ja/docs/Web/API/Window/alert
-if (input == '晴れ') {
+if (input === '晴れ') {
     alert('今日は気持ちの良い一日になりそうですね！');
-} else if (input == 'くもり') {
+} else if (input === 'くもり') {
     alert('どんよりした天気ですが今日も一日がんばりましょう！');
-} else if (input == '雨') {
+} else if (input === '雨') {
     alert('家でゆっくり過ごすのも良いと思いますよ！');
 } else {
     alert('晴れ・くもり・雨以外の文字列には対応していません。');
